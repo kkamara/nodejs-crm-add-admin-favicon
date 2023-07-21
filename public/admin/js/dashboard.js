46f36a99805896d77e06aaf5d4d6ff68eac5c79d
kkamara <kamaracomputers@gmail.com>
2023-07-21 13:23:57 +0100

    Add admin/dashboard.js

---
+++ public/admin/js/dashboard.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const run = async () => {
+  let res = null;
+  try {
+    const token = localStorage.getItem('auth');
+    if (null === token) {
+      window.location.href = domain+'/admin';
+    }
+    res = await axios.post(
+      domain+'/admin/authenticate', 
+      null,
+      {
+        headers: {
+          'Authorization': `Basic ${token}`,
+        },
+      },
+    );
+    data = res.data;
+    console.log(data);
+  } catch (err) {
+    let message = err.message;
+    if (err.response && err.response.data.message) {
+      message = err.response.data.message;
+    }
+    if (err.response && err.response.data.errors) {
+      message = err.response.data.errors;
+    }
+    console.log(message);
+  }
+};
+
+run();
