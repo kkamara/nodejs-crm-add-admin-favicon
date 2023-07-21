e2393cfb7b5b1ec568a4a87cfd8bcab8cb2a1e7b
kkamara <kamaracomputers@gmail.com>
2023-07-21 12:06:22 +0100

    Add admin dashboard page with vali-admin theme

---
+++ src/routes/admin/dashboard.js
@@ -0,0 +1,44 @@
+'use strict';
+const express = require('express');
+const { QueryTypes, } = require('sequelize');
+const deepClone = require('deep-clone');
+const config = require('../../config');
+const db = require('../../database');
+const { 
+  validateAuthenticate,
+  authenticate,
+  getNewToken,
+} = require('../../models/user');
+
+const dashboard = express.Router();
+
+dashboard.get('/', async (req, res) => {
+  req.session.page = { title: 'Admin Dashboard', };
+  req.session.auth = null;
+
+  await new Promise((resolve, reject) => {
+    req.session.save(function(err) {
+      if (err) {
+        console.log(err)
+        return reject(err);
+      }
+      resolve()
+    });
+  });
+  
+  const newSession = { page: req.session.page, auth: req.session.auth, };
+  const session = deepClone(newSession);
+  await new Promise((resolve, reject) => {
+    req.session.destroy(function(err) {
+      if (err) {
+        console.log(err)
+        return reject(err);
+      }
+      resolve();
+    });
+  });
+  
+  return res.render('admin/dashboard.pug');
+});
+
+module.exports = dashboard;
--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -1,9 +1,11 @@
 'use strict';
 const express = require('express');
 const login = require('./login');
+const dashboard = require('./dashboard');
 
 const router = express.Router();
 
 router.use('/', login);
+router.use('/dashboard', dashboard);
 
 module.exports = router;
